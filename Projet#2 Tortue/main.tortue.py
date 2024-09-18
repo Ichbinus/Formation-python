ccf5325e459dde7b13ac53eaef0441b3c05ad3cb
Maxime Tertrais <maxime.tertrais@gmail.com>
2024-09-18 14:49:28 +0200

    escalier tortue

--- Projet#2 Tortue/main.tortue.py
+++ Projet#2 Tortue/main.tortue.py
@@ -2,4 +2,14 @@ import turtle
 
 t=turtle.Turtle()
 
+def escalier(taille,nb):
+    for i in range (0,nb):
+        t.forward(taille)
+        t.left (90)
+        t.forward(taille)
+        t.right (90)
+    t.forward(taille)
+
+escalier (30,5)
+
 turtle.done()
