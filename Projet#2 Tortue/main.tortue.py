516c03e4ec7fe4c481e38e8cf1f01a7480df9082
Maxime Tertrais <maxime.tertrais@gmail.com>
2024-09-18 15:04:07 +0200

    fin projet 2

--- Projet#2 Tortue/main.tortue.py
+++ Projet#2 Tortue/main.tortue.py
@@ -2,14 +2,28 @@ import turtle
 
 t=turtle.Turtle()
 
-def escalier(taille,nb):
+"""def escalier(taille,nb):
     for i in range (0,nb):
         t.forward(taille)
         t.left (90)
         t.forward(taille)
         t.right (90)
     t.forward(taille)
+"""
+def carre(taille):
+    for i in range (0,4):
+        t.forward(taille)
+        t.left (90)
+        
 
-escalier (30,5)
+def carres (taille_depart,nb):
+    for i in range(0, nb):
+        taille = (i+1)*(i+1)*taille_depart
+        carre(taille)
+
+#escalier (30,5)
+#carre (30,5)
+
+carres (10,10)
 
 turtle.done()
